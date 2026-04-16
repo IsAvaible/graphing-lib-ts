f06be7c28a02fca7465f719cfeef3b20d696c483
Simon Felix Conrad <simon.felix.conrad@proton.me>
2026-04-16 12:11:15 +0200

    feat(ui): add shadcn

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,6 +1,8 @@
 import React, { type ChangeEvent } from "react";
 import { Graph } from "../core/Graph";
 import { readGraphFromFileBrowser } from "../io/browser-reader.ts";
+import { Button } from "@/components/ui/button.tsx";
+import { UploadIcon } from "lucide-react";
 
 interface TopBarProps {
   onGraphLoaded: (graph: Graph<number>) => void;
@@ -16,25 +18,26 @@ export const TopBar: React.FC<TopBarProps> = ({ onGraphLoaded }) => {
   };
 
   return (
-    <div className="flex items-center justify-between px-6 py-4 bg-gray-900 text-white shadow-md z-10 relative">
+    <div className="flex items-center justify-between px-6 py-4 shadow-sm z-10 relative">
       <h1 className="text-xl font-bold tracking-wider">SC² Graphing Library</h1>
 
       <div className="flex items-center gap-4">
-        <label className="cursor-pointer bg-indigo-600 hover:bg-indigo-500 text-white px-3 py-2 rounded-md text-sm font-medium transition-colors">
-          Upload
-          <input
-            type="file"
-            accept=".txt"
-            className="hidden"
-            onChange={handleFileUpload}
-          />
-        </label>
-        <button
-          className="bg-gray-700 hover:bg-gray-600 px-3 py-2 rounded-md text-sm font-medium transition-colors"
-          onClick={() => alert("BFS Animation not implemented yet.")}
-        >
+        <Button asChild variant="outline">
+          <label>
+            <UploadIcon />
+            Upload
+            <input
+              type="file"
+              accept=".txt"
+              className="hidden"
+              onChange={handleFileUpload}
+            />
+          </label>
+        </Button>
+
+        <Button onClick={() => alert("BFS Animation not implemented yet.")}>
           Run BFS
-        </button>
+        </Button>
       </div>
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
+import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()]
+  plugins: [tailwindcss(), react()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 });
